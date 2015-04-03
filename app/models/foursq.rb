7c2d53096cbe89a79ac53149dad8843c9b0d8596
Vienne Chen <vienne.chen@gmail.com>
2015-04-03 14:45:54 -0400

    FOURSQUARE VIENNE

--- app/models/foursq.rb
+++ app/models/foursq.rb
@@ -1,21 +1,29 @@
-require 'httparty'
+# require 'httparty'
 
-var baseURL = "https://api.foursquare.com/v2/venues/search"
+# var baseURL = "https://api.foursquare.com/v2/venues/search"
 
-var version = "&v=20150223";
+# var version = "&v=20150223";
 
 
- 		name: venue["name"],
-    website: venue["url"],
-    checkins: venue["stats"]["checkinsCount"],
-    users: venue["stats"]["usersCount"],
-    usersHereNow: venue["hereNow"]["count"],
-    hereNowSummary: venue["hereNow"]["summary"]
+#  		name: venue["name"],
+#     website: venue["url"],
+#     checkins: venue["stats"]["checkinsCount"],
+#     users: venue["stats"]["usersCount"],
+#     usersHereNow: venue["hereNow"]["count"],
+#     hereNowSummary: venue["hereNow"]["summary"]
 
-    https://api.foursquare.com/v2/venues/search
-  ?client_id=CLIENT_ID
-  &client_secret=CLIENT_SECRET
-  &v=20130815
-  &ll=40.7,-74
-  &query=sushi
+#     https://api.foursquare.com/v2/venues/search
+#   ?client_id=CLIENT_ID
+#   &client_secret=CLIENT_SECRET
+#   &v=20130815
+#   &ll=40.7,-74
+#   &query=sushi
+
+#   https://api.foursquare.com/v2/venues/search
+#   ?client_id=CLIENT_ID
+#   &client_secret=CLIENT_SECRET
+#   &v=20130815
+#   &near=40.7393080,-73.9894290
+
+
 
